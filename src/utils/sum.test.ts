9942a769e761767dc319ded94a2c53d1e3a5bc1d
neko-no <noran4ko@gmail.com>
2026-07-05 15:23:07 +0900

    feat: テストをAAAパターンでの実装に変更 #4

--- src/utils/sum.test.ts
+++ src/utils/sum.test.ts
@@ -1,11 +1,29 @@
 import { sum } from './sum';
 
 describe('sum関数の単体テスト', () => {
-  it('2つの数値の合計を返すこと', () => {
-    expect(sum([1, 2])).toBe(3);
+  it('空の配列を私た場合、結果は0になる', () => {
+    const number: number[] = [];
+
+    const result = sum(number);
+
+    expect(result).toBe(0);
+  });
+
+  it('複数の正の数値を渡した場合、正しく合計を返す', () => {
+    const numbers = [1, 2, 3];
+    const result = sum(numbers);
+    expect(result).toBe(6);
   });
 
   it('負の値を含めても正しく計算できること', () => {
-    expect(sum([-5, 5])).toBe(0);
+    const numbers = [-1, 5, -3];
+    const result = sum(numbers);
+    expect(result).toBe(1);
+  });
+
+  it('要素が1つだけの配列はそのまま返す', () => {
+    const numbers = [42];
+    const result = sum(numbers);
+    expect(result).toBe(42);
   });
 });
