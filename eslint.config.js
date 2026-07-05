addfd0e0cef130d47c76973d61b1e1d66dd150d0
neko-no <noran4ko@gmail.com>
2026-07-05 11:41:08 +0900

    feat: stylelint導入 #2

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules', 'build']),
@@ -12,6 +13,7 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      prettierConfig,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
