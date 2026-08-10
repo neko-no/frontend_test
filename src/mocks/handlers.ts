e082fe9318f39e8c5d3f107fea3cd884e75edb9f
neko-no <101441574+neko-no@users.noreply.github.com>
2026-08-10 17:44:24 +0900

    feat: mswにてクエリパラメーターを取得して処理を変更するハンドラーを定義 #14

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -7,5 +7,18 @@ export const handlers = [
       {id: 2, name: "鈴木花子", email: "suzuki@example.com"},
       {id: 3, name: "佐藤次郎", email: "sato@example.com"},
     ])
-  })
+  }),
+  http.get("/api/users/:userId", ({params}) => {
+    const {userId} = params;
+
+    if(userId === "1"){
+      return HttpResponse.json({
+        id: 1,
+        name: "田中太郎",
+        email: "tanaka@example.com",
+      })
+    }
+
+    return new HttpResponse(null, {status: 404});
+  }),
 ]
