a2bb9f35fd9ca24a0b7c8ade0e6823f3cd380d63
neko-no <101441574+neko-no@users.noreply.github.com>
2026-08-10 18:25:13 +0900

    feat: storybook上で直接ハンドラーを定義 #14

--- src/components/UserList/UserList.stories.tsx
+++ src/components/UserList/UserList.stories.tsx
@@ -11,7 +11,23 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 
-export const Default: Story = {};
+export const Default: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("/api/users", async () => {
+          await delay(500);
+
+          return HttpResponse.json([
+            {id: 1, name: "田中太郎", email: "tanaka@example.com"},
+            {id: 2, name: "鈴木花子", email: "suzuki@example.com"},
+            {id: 3, name: "佐藤次郎", email: "sato@example.com"},
+          ])
+        })
+      ]
+    }
+  }
+};
 
 
 export const ErrorState: Story = {
@@ -31,6 +47,7 @@ export const EmptyState: Story = {
     msw: {
       handlers: [
         http.get("/api/users", () => {
+          await delay(300);
           return HttpResponse.json([]);
         })
       ]
