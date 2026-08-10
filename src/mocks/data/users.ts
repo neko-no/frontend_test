c6ac9d39e1678c4380ff956b5ed591c90ed8c21e
neko-no <101441574+neko-no@users.noreply.github.com>
2026-08-10 17:53:49 +0900

    refactor: mockレスポンスの一部を切り出して，handler側で利用 #14

---
+++ src/mocks/data/users.ts
@@ -0,0 +1,5 @@
+export const mockUsers = [
+  {id:1, name: "田中太郎", email: "tanaka@example.com"},
+  {id:2, name: "鈴木花子", email: "suzuki@example.com"},
+  {id:3, name: "佐藤次郎", email: "sato@example.com"},
+];
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,25 +1,21 @@
 import { hhtp, HttpResponse } from "msw";
+import { mockUsers} from "./data/users";
+
 
 export const handlers = [
   http.get("/api/users", () => {
-    return HttpResponse.json([
-      {id: 1, name: "田中太郎", email: "tanaka@example.com"},
-      {id: 2, name: "鈴木花子", email: "suzuki@example.com"},
-      {id: 3, name: "佐藤次郎", email: "sato@example.com"},
-    ])
+    return HttpResponse.json(mockUsers);
   }),
   http.get("/api/users/:userId", ({params}) => {
     const {userId} = params;
 
-    if(userId === "1"){
-      return HttpResponse.json({
-        id: 1,
-        name: "田中太郎",
-        email: "tanaka@example.com",
-      })
+    const user = mockUsers.find((u) => u.id === Number(userId));
+
+    if(!user){
+      return new HttpResponse(null, {status: 404})
     }
 
-    return new HttpResponse(null, {status: 404});
+    return HttpResponse.json(user);
   }),
   http.post("/api/users", async({request}) => {
     const newUser = await request.json();
